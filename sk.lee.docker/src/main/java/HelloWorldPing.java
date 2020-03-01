c2d20f9c1d6bf3be55407d2ee3614c4a60de09f4
sk <sk@sk-HP-Pavilion-Laptop-15-cc1xx>
2020-02-29 21:46:22 -0600

    changed class

--- sk.lee.docker/src/main/java/HelloWorldPing.java
+++ sk.lee.docker/src/main/java/HelloWorldPing.java
@@ -2,7 +2,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Lee Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
 	}
